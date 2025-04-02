1d47b61f9ff2cea9d2359c1e4f6e77e7f99279ab
Maga <mcmagakrytoy@gmail.com>
2025-04-02 17:24:45 +0300

    создание файла database и article, вынес database в отдельный файл

---
+++ database.py
@@ -0,0 +1,28 @@
+from article import dataclass
+
+@dataclass
+class Article:
+    title: str
+    content: str
+    image: str| None = None
+
+class Database:
+    articles = []
+
+    @staticmethod
+    def save(article: Article):
+        if Database.find_article_by_title(article.title) is not None:
+            return False
+        
+        Database.articles.append(article)
+        return True
+    
+    @staticmethod
+    def get_all_articles():
+        return Database.articles
+    
+    def find_article_by_title(title: str):
+        for article in Database.articles:
+            if article.title == title:
+                return article
+        return None
